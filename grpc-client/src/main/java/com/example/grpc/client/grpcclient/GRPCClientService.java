c2b07ebb53a19ec104eacbe9cc9f3c6092ba926b
jamieives-hub <60115745+jamieives-hub@users.noreply.github.com>
2022-03-10 14:20:27 +0000

    uncommeted

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
@@ -13,10 +13,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Service;
 
 
-// package com.example.uploadingfiles;
+package com.example.uploadingfiles;
 
-// import org.springframework.boot.SpringApplication;
-// import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @Service
@@ -34,16 +34,16 @@ public class GRPCClientService {
 		return helloResponse.getPong();
     }
 	// upload
-	// @PostMapping("/")
-	// public String handleFileUpload(@RequestParam("file") MultipartFile file,
-	// 		RedirectAttributes redirectAttributes) {
+	@PostMapping("/")
+	public String handleFileUpload(@RequestParam("file") MultipartFile file,
+			RedirectAttributes redirectAttributes) {
 
-	// 	storageService.store(file);
-	// 	redirectAttributes.addFlashAttribute("message",
-	// 			"You successfully uploaded " + file.getOriginalFilename() + "!");
+		storageService.store(file);
+		redirectAttributes.addFlashAttribute("message",
+				"You successfully uploaded " + file.getOriginalFilename() + "!");
 
-	// 	return "redirect:/";
-	// }
+		return "redirect:/";
+	}
     public String add(){
 		ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost",9090)
 		.usePlaintext()
@@ -63,23 +63,23 @@ public class GRPCClientService {
 		String resp= A.getC00()+" "+A.getC01()+"<br>"+A.getC10()+" "+A.getC11()+"\n";
 		return resp;
     }
-	// public String mult(){
-	// 	ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost",9090)
-	// 	.usePlaintext()
-	// 	.build();
-	// 	MatrixServiceGrpc.MatrixServiceBlockingStub stub
-	// 	 = MatrixServiceGrpc.newBlockingStub(channel);
-	// 	MatrixReply A=stub.multiplyBlock(MatrixRequest.newBuilder()
-	// 		.setA00(1)
-	// 		.setA01(2)
-	// 		.setA10(5)
-	// 		.setA11(6)
-	// 		.setB00(1)
-	// 		.setB01(2)
-	// 		.setB10(5)
-	// 		.setB11(6)
-	// 		.build());
-	// 	String resp= A.getC00()+" "+A.getC01()+"<br>"+A.getC10()+" "+A.getC11()+"\n";
-	// 	return resp;
-	// }
+	public String mult(){
+		ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost",9090)
+		.usePlaintext()
+		.build();
+		MatrixServiceGrpc.MatrixServiceBlockingStub stub
+		 = MatrixServiceGrpc.newBlockingStub(channel);
+		MatrixReply A=stub.multiplyBlock(MatrixRequest.newBuilder()
+			.setA00(1)
+			.setA01(2)
+			.setA10(5)
+			.setA11(6)
+			.setB00(1)
+			.setB01(2)
+			.setB10(5)
+			.setB11(6)
+			.build());
+		String resp= A.getC00()+" "+A.getC01()+"<br>"+A.getC10()+" "+A.getC11()+"\n";
+		return resp;
+	}
 }
