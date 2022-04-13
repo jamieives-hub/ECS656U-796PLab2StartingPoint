2890802264f8267867392fcf9230f5a76bd57006
jamieives-hub <60115745+jamieives-hub@users.noreply.github.com>
2022-04-13 15:50:35 +0100

    changed

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/PingPongEndpoint.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/PingPongEndpoint.java
@@ -6,8 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
@@ -30,9 +32,9 @@ public class PingPongEndpoint {
 	public String add() {
 		return grpcClientService.add();
 	}
-	@PostMapping("/")
-	public FileUploadResponse fileUpload(@RequestParam("file") MultipartFile file) throws IllegalStateException, IOException {
+	@PostMapping("/upload")
+	public String fileUpload(@RequestParam("file") MultipartFile file) throws IllegalStateException, IOException {
 		System.out.println("Upload Test");
-		return grpcClientService.handleFileUpload(file);
+		return grpcClientService.handleFileUpload(file, null);
 	}
 }
