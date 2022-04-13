f23af5b5ff5419b117f49a4130817efd124d691c
jamieives-hub <60115745+jamieives-hub@users.noreply.github.com>
2022-04-13 20:25:20 +0100

    controller

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/PingPongEndpoint.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/PingPongEndpoint.java
@@ -20,7 +20,7 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
-@RestController
+@Controller
 public class PingPongEndpoint {    
 
 	GRPCClientService grpcClientService;    
